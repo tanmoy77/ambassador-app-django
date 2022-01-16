0867e65347121e1d65e23ebc5a4d90ce31eea937
Tanmoy Joy <dukejay97@gmail.com>
2022-01-17 00:22:36 +0600

    added redis cache

--- ambassador/views.py
+++ ambassador/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from core.models import Product
+from .serializers import ProductSerializer
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView 
+from rest_framework.response import Response
+from django.core.cache import cache 
 
-# Create your views here.
+
+class ProductFrontendAPIView(APIView):
+    @method_decorator(cache_page(60 * 60 * 2, key_prefix='products_frontend'))
+    def get(self, _):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductBackendAPIView(APIView):
+    def get(self, _):
+        products = cache.get('products_backend')
+
+        if not products:
+            products = list(Product.objects.all())
+            cache.set('products_backend', products, timeout=60*30) #30 min
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
